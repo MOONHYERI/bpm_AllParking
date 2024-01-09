4481bb229c9a3d5bf8875fd50a8a8fd4756c9615
MOONHYERI <a6967829@naver.com>
2024-01-09 23:23:19 +0900

    수정

---
+++ src/main/java/kr/allparking/bpm_AllParking/entity/KakaologinApplication.java
@@ -0,0 +1,13 @@
+package kr.allparking.bpm_AllParking.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaologinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaologinApplication.class, args);
+    }
+
+}
